e532f13bab0c42892eed5fb9f166d6f843338568
Natalialocatelli <natalialocatellib@gmail.com>
2022-08-16 11:48:29 -0300

    Funcoes dos posts

--- The Social Network Challenge/The Social Network Challenge/ViewModel/API.swift
+++ The Social Network Challenge/The Social Network Challenge/ViewModel/API.swift
@@ -10,18 +10,18 @@ import Foundation
 
 // implemented api communication
 func getUsers(completion: @escaping (([Users]) -> ())) {
-    let url = URL(string: "http://adaspace.local/users")!
-    
+    let url = URL(string: "http://adaspace.local/users%22)!
+
     let task = URLSession.shared.dataTask(with: url){
-        
+
         data, response, error in
         guard let dataResponse = data else {return }
-        
+
         do {let allUsers = try JSONDecoder().decode([Users].self, from: dataResponse)
-//            print("object posts: \(posts)")
+//            print("object posts: (posts)")
             completion(allUsers)
         } catch let error{
-            print("ERROR: \(error)")
+            print("ERROR: (error)")
         }
     }
 
@@ -29,19 +29,34 @@ func getUsers(completion: @escaping (([Users]) -> ())) {
 }
 
 func getPosts(completion: @escaping (([Posts]) -> ())){
-    let url = URL(string: "http://adaspace.local/posts")!
+    let url = URL(string: "http://adaspace.local/posts%22)!
     let task = URLSession.shared.dataTask(with: url) { data, response, error in
-        
+
         guard let dataResponse = data else {return}
-        
+
         do {
             let allPosts = try JSONDecoder().decode([Posts].self, from: dataResponse)
             completion(allPosts)
         }
-        
+
         catch let error {
-            print("ERROR\(error)")
+            print("ERROR: (error)")
         }
     }
     task.resume()
+}
+
+func separatePostsWithWithoutMedia (_ posts: [Posts]) -> ([Posts], [Posts]){
+    var postsWithMedia:[Posts] = []
+    var textPosts: [Posts] = []
+    for post in posts{
+        if post.media == nil {
+            textPosts.append(post)
+        }
+        else{
+            postsWithMedia.append(post)
+        }
+    }
+
+    return (postsWithMedia, textPosts)
 }
